ceb836cb03d5289cc85c9c7681ddde2941cb2ed4
DanielBabenko <danielbabenko606@yandex.ru>
2024-06-04 19:21:35 +0300

    52+ : Test update

--- src/test/java/utils/CheckerTest.java
+++ src/test/java/utils/CheckerTest.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.junit.Test;
 
+import static java.lang.Math.*;
 import static org.junit.Assert.*;
 
 public class CheckerTest {
@@ -161,5 +162,50 @@ public class CheckerTest {
         assertFalse(Checker.getIsValid(x, y, highBorderR1));
         assertFalse(Checker.getIsValid(x, y, highBorderR));
         assertTrue(Checker.getIsValid(x, y, highBorderR2));
+    }
+
+    @Test
+    public void checkIfOnSectorEdge() {
+        assertTrue(Checker.checkHit((float)-sqrt(2),(float)sqrt(2),2));
+    }
+
+    @Test
+    public void checkIfOnSector() {
+        assertTrue(Checker.checkHit(-1,1,2));
+    }
+
+    @Test
+    public void checkIfNotOnSector() {
+        assertFalse(Checker.checkHit(-2,2,2));
+    }
+
+    @Test
+    public void checkIfOnRectEdge() {
+        assertTrue(Checker.checkHit(-1,-2,2));
+    }
+
+    @Test
+    public void checkIfOnRect() {
+        assertTrue(Checker.checkHit(-0.9,-0.9,2));
+    }
+
+    @Test
+    public void checkIfNotOnRect() {
+        assertFalse(Checker.checkHit(-1.1,-2.1,1));
+    }
+
+    @Test
+    public void checkIfOnTriangleEdge() {
+        assertTrue(Checker.checkHit(0.75,-0.75,3));
+    }
+
+    @Test
+    public void checkIfOnTriangle() {
+        assertTrue(Checker.checkHit(0.5,-0.6,3));
+    }
+
+    @Test
+    public void checkIfNotOnTriangle() {
+        assertFalse(Checker.checkHit(1.0000001,-0.5,3));
     }
 }
